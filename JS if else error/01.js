ec8104d28881c93a535f1577e078c766b6d17ca3
AungKyawChing <122950055+AungKyawChing@users.noreply.github.com>
2023-05-20 12:54:17 +0600

    Create 01.js
    in if else Condition, when i input string and say that the string is equal of 5 or any number the system is agreed with it but it should be go for else Condition. Does It JS Bugs??

---
+++ JS if else error/01.js
@@ -0,0 +1,10 @@
+var number = "Aung";
+if (number<5) {
+    console.log("The Number is Smaller than 5.");
+}else if(number>5){
+    console.log("The Number is Bigger than 5.");
+}else if(number=5){
+    console.log("The Number is equal of 5.");// it is returning it. why not showing System Error?
+}else{
+    console.log("System Error");
+}
